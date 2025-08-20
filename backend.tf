f4ec64f7aca9b1cfe20175db2ce8ea5b818e3fd8
srachendra <30568096+srachendra@users.noreply.github.com>
2025-08-20 19:30:41 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,8 +4,6 @@ terraform {
     storage_account_name  = "royalstorageaccount1"
     container_name        = "royalcontainer"
     key                   = "terraform.tfstate"
-    use_azuread_auth      = true
-    subscription_id       = "2b1551ba-06d4-4000-8ff6-98b00d5ffa59"
-    tenant_id             = "6e437120-99de-480f-bca4-0a53d8ac8fd6"
+    subscription_id       = "2b1551ba-06d4-4000-8ff6-98b00d5ffa59
   }
 }
