32133c8bdb0ed6951b50619dc5bcbf2eea74070b
srachendra <30568096+srachendra@users.noreply.github.com>
2025-08-20 11:41:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,22 @@
-#######################
 # Provider
-#######################
+##############
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
 
-#######################
+
+
 # Resource Group
 #######################
 resource "azurerm_resource_group" "rg" {
@@ -106,3 +117,5 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   disable_password_authentication = true
 }
+
+
